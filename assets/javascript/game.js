8c9fc1578a523e244090edc505a9dd7faad825eb
MCorey <mjcorey57@gmail.com>
2018-10-19 23:57:29 -0600

    updated code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,74 @@
+ var wins = 0;
+ var loss = 0;
+ var turns = 10;
  
+ var incorrectCount = 0;
+ var correctCount = 0;
+ var incorrectArray = [];
+ var correctArray = [];
+ 
+ // Array of player names.
+ var playerNames = ["elway", "sharp", "davis", "atwater", "manning", "mccaffrey", "mecklenburg", "gradishar", "jackson", "little", "zimmerman", "tripucka", "miller"];
+ var totalPlayerNames = playerNames.length;
+ var playerNameIndex = Math.floor((Math.random() * 10) + 1) % totalPlayerNames;
+ var playerName = playerNames[playerNameIndex];
+ //var correctChars = playerName.split('');
  
 
-    var wins = 0;
-    var loss = 0;
-    var turns = 12;
-    var incorrectCount = 0;
-    var correctCount = 0;
-    var incorrectArray =[];
-    var correctArray =[];
+ for (var i = 0; i < playerName.length; i++) {
+   var underscore = playerName.replace(/\D/g, " _ ");
+ };
  
- //function selectWord() { 
-  // Array of player names.
-  var playerNames = ["elway", "sharp", "davis", "atwater", "manning", "mccaffrey", "mecklenburg", "gradishar", "jackson", "little", "zimmerman", "tripucka", "miller"];
-  var totalPlayerNames = playerNames.length;
-  var playerNameIndex = Math.floor((Math.random() * 10) + 1) % totalPlayerNames;
-  var playerName = playerNames[playerNameIndex];
-  
-  for(var i = 0; i < playerName.length; i++) {
-  var underscore = playerName.replace(/\D/g, " _ ");
-};
-  //document.getElementById("gameWord").innerHTML = playerName;
-  document.getElementById("gameWord").innerHTML = underscore;
-  
- //};
+ document.getElementById("gameWord").innerHTML = underscore;
 
  document.onkeyup = function (event) {
-  var letter = event.key.toLowerCase();
-  checkLetter(letter);
+   if(event.which <= 90 && event.which >= 65){ //checks to see if key is a letter.
+   var letter = event.key.toLowerCase();
+   checkLetter(letter);
+   turnsRemaining = (turns) - (correctCount + incorrectCount );
+   document.getElementById("lblTurnsRemain").innerHTML = turnsRemaining;
+   if(turnsRemaining == 0){
+     loss++;
+     document.getElementById("status").innerHTML = "Sorry you lose."
+     document.getElementById("lblLoses").innerHTML = loss;
+   };
+    
+   };
  };
 
- function checkLetter(letter){
+ function checkLetter(letter) {
+   //converts letter to a regular expression. Use the 'g' flag to find all letters that match
+   //the same letter pressed by the user
+   var re = new RegExp(letter, 'g');
+   var res = playerName.match(re);
+    console.log(res);
+   //update array of incorrect letters
+   if (res == null) {
+     if (incorrectArray.includes(letter)) { //Check to see if letter already exist in array. If not add to array
 
-  //converts letter into a regular expression with the 'g' flag to find all letters
-  //that are the same as the letter pressed by the user
-  var re = new RegExp(letter, 'g');
-  var res = playerName.match(re);
-  
-  //update array of incorrect letters
-  if(res == null){
-    incorrectArray.push(letter);
-    incorrectCount = incorrectArray.length;
-    document.getElementById("incorrectGuess").value = incorrectCount;
-    console.log(incorrectCount);
+     } else {
+       incorrectArray.push(letter);
+       incorrectCount = incorrectArray.length;
+       document.getElementById("incorrectGuess").innerHTML = incorrectCount;
+       //adds letters from the incorrectArray to lblIncorrectLetter using .join method
+       document.getElementById("lblIncorrectLetter").innerHTML = incorrectArray.join("  ");
+       if(incorrectCount > turns){
+        
+      }
+     };
+     //update array of correct letters
+   } else {
+     if (correctArray.includes(letter)) { //Check to see if letter already exist in array. If not add to array
 
-  //update array of correct letters
-}else{
-  
-  correctArray.push(letter);
-  correctCount = correctArray.length;
-  document.getElementById("correctGuess").value =  correctCount;
-  //console.log(correctLetters);
-};
-  
-
-
- // console.log(res);
-  //console.log(playerName);
-  
-  
+     } else {
+       correctArray.push(letter);
+       correctCount = correctArray.length;
+       document.getElementById("correctGuess").innerHTML = correctCount;
+       if(correctCount.length == (playerName.length)){
+        document.getElementById("status").innerHTML = "TOUCHDOWN!! You win!"
+        wins++
+        document.getElementById("lblWins").innerHTML = wins;
+       }; 
+     };  
+   };
  };
-
-
- 
